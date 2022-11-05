6671b75517876e2241856f1ebf2c5cb734aa0327
Borsh Vladislav Aleksandrovich <ext.vborsh@tcsbank.ru>
2022-11-05 11:12:08 +0200

    add env vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  firebase: {
+    databaseURL: 'https://cell-editor-default-rtdb.firebaseio.com/',
+    measurementId: '<your-measurement-id>',
+    apiKey: 'AIzaSyDJTu3As6Ce5QjpL_jIk8P1zgltsYEKiWI',
+    authDomain: 'cell-editor.firebaseapp.com',
+    projectId: 'cell-editor',
+    storageBucket: 'cell-editor.appspot.com',
+    messagingSenderId: '741229528151',
+    appId: '1:741229528151:web:8bf3c6ce8ccec8c8bf8f12',
+  },
 };
